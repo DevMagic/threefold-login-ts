35d35bb4a001ea4028c75c8e60253c793cf5c5b7
Claudio Oliveira <claudio_pg-sp@hotmail.com>
2022-03-09 14:47:52 -0300

    update version, removed unused import

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import { ThreefoldErrorException } from "./core/threefold-error-exception";
 import { ThreefoldLogin } from "./core/threefold-login";
-import { ThreefoldUtils } from "./core/threefold-utils";
 import { CryptoService } from "./crypto/crypto.service";
 
 export {
